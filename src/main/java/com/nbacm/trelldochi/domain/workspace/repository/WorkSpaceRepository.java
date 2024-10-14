847cafe09432363513ed441d1c42436ffc7410bb
Woojin1123 <boom175839@gmail.com>
2024-10-14 17:15:37 +0900

    feat : 워크스페이스 단건/다건 조회 기능 구현

---
+++ src/main/java/com/nbacm/trelldochi/domain/workspace/repository/WorkSpaceRepository.java
@@ -0,0 +1,26 @@
+package com.nbacm.trelldochi.domain.workspace.repository;
+
+import com.nbacm.trelldochi.domain.workspace.entity.WorkSpace;
+import com.nbacm.trelldochi.domain.workspace.exception.WorkSpaceNotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface WorkSpaceRepository extends JpaRepository<WorkSpace, Long>, WorkSpaceQueryDslRepository {
+    @Query("SELECT w FROM WorkSpace w " +
+            "JOIN WorkSpaceMember wm " +
+            "ON w.id = wm.id " +
+            "JOIN User u " +
+            "ON wm.user.id = u.id " +
+            "WHERE u.email = :email " +
+            "AND w.id = :id "
+    )
+    Optional<WorkSpace> findByUserEmailAndId(@Param("email") String email,
+                                             @Param("id") Long Id);
+
+    default WorkSpace findByUserEmailAndIdOrElseThrow(String email,Long id) {
+        return findByUserEmailAndId(email,id).orElseThrow(()-> new WorkSpaceNotFoundException("워크스페이스를 찾을 수 없습니다."));
+    }
+}
---
+++ src/main/java/com/nbacm/trelldochi/domain/workspace/service/WorkSpaceServiceImpl.java
@@ -0,0 +1,34 @@
+package com.nbacm.trelldochi.domain.workspace.service;
+
+import com.nbacm.trelldochi.domain.user.entity.User;
+import com.nbacm.trelldochi.domain.user.repository.UserRepository;
+import com.nbacm.trelldochi.domain.workspace.dto.WorkSpaceResponseDto;
+import com.nbacm.trelldochi.domain.workspace.repository.WorkSpaceMemberRepository;
+import com.nbacm.trelldochi.domain.workspace.repository.WorkSpaceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class WorkSpaceServiceImpl implements WorkSpaceService {
+
+    private final UserRepository userRepository;
+    private final WorkSpaceRepository workSpaceRepository;
+    private final WorkSpaceMemberRepository workSpaceMemberRepository;
+
+    @Override
+    public Page<WorkSpaceResponseDto> getWorkSpaces(String email, int page, int size) {
+        User user = userRepository.findByEmailOrElseThrow(email);
+        Pageable pageable = PageRequest.of(page - 1, size);
+
+        return workSpaceRepository.findWorkSpacesByUserId(user.getId(), pageable);
+    }
+
+    @Override
+    public WorkSpaceResponseDto getWorkSpace(String email, Long workSpaceId) {
+        return new WorkSpaceResponseDto(workSpaceRepository.findByUserEmailAndIdOrElseThrow(email, workSpaceId));
+    }
+}
