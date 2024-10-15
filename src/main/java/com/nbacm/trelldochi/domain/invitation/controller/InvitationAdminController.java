ad453ad2db5f91c27640e3a711195dca28ba46f0
Woojin1123 <boom175839@gmail.com>
2024-10-15 13:23:28 +0900

    fix : 인증객체 수정 & Controller맵핑 수정 & 잘못된 메서드명 수정

--- src/main/java/com/nbacm/trelldochi/domain/invitation/controller/InvitationAdminController.java
+++ src/main/java/com/nbacm/trelldochi/domain/invitation/controller/InvitationAdminController.java
@@ -1,26 +1,24 @@
 package com.nbacm.trelldochi.domain.invitation.controller;
 
 import com.nbacm.trelldochi.domain.common.advice.ApiResponse;
-import com.nbacm.trelldochi.domain.common.dto.AuthUser;
-import com.nbacm.trelldochi.domain.invitation.service.InvitationAdminServiceImpl;
+import com.nbacm.trelldochi.domain.common.dto.CustomUserDetails;
 import com.nbacm.trelldochi.domain.invitation.dto.InvitationRequestDto;
 import com.nbacm.trelldochi.domain.invitation.dto.InvitationResponseDto;
+import com.nbacm.trelldochi.domain.invitation.service.InvitationAdminServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/admin/workspaces")
 public class InvitationAdminController {
 
     private final InvitationAdminServiceImpl invitationAdminService;
     @PostMapping("/{workspaceId}/invitations")
     public ResponseEntity<ApiResponse<InvitationResponseDto>> inviteMember(
-            @AuthenticationPrincipal AuthUser authUser,
+            @AuthenticationPrincipal CustomUserDetails authUser,
             @PathVariable Long workspaceId,
             @RequestBody InvitationRequestDto requestDto
     ) {
@@ -37,7 +35,7 @@ public class InvitationAdminController {
 
     @PostMapping("/{workspaceId}/invitations/{invitationId}/cancle")
     public ResponseEntity<ApiResponse<String>> cancleInvitation(
-            @AuthenticationPrincipal AuthUser authUser,
+            @AuthenticationPrincipal CustomUserDetails authUser,
             @PathVariable Long invitationId
     ) {
         invitationAdminService.cancleInvitation(authUser.getEmail(), invitationId);
--- src/main/java/com/nbacm/trelldochi/domain/invitation/controller/InvitationController.java
+++ src/main/java/com/nbacm/trelldochi/domain/invitation/controller/InvitationController.java
@@ -1,9 +1,42 @@
 package com.nbacm.trelldochi.domain.invitation.controller;
 
+import com.nbacm.trelldochi.domain.common.advice.ApiResponse;
+import com.nbacm.trelldochi.domain.common.dto.CustomUserDetails;
+import com.nbacm.trelldochi.domain.invitation.service.InvitationServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/admin/workspaces")
 public class InvitationController {
+
+    private final InvitationServiceImpl invitationService;
+
+    @PostMapping("/{workspaceId}/invitations/{invitationId}/accept")
+    public ResponseEntity<ApiResponse<String>> accpetInvitation(
+            @AuthenticationPrincipal CustomUserDetails authUser,
+            @PathVariable Long invitationId
+    ) {
+        invitationService.acceptInvitation(authUser.getEmail(), invitationId);
+        return ResponseEntity.ok(ApiResponse.success(
+                "ACCEPT SUCCESS"
+        ));
+    }
+
+    @PostMapping("/{workspaceId}/invitations/{invitationId}/reject")
+    public ResponseEntity<ApiResponse<String>> rejectInvitation(
+            @AuthenticationPrincipal CustomUserDetails authUser,
+            @PathVariable Long invitationId
+    ) {
+        invitationService.rejectInvitation(authUser.getEmail(), invitationId);
+        return ResponseEntity.ok(ApiResponse.success(
+                "REJECT SUCCESS"
+        ));
+    }
 }
